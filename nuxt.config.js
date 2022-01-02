a6d2812c6d976623240301cc191dcfd2653b3e03
Mikhail Uglianskii <mikhail.uglianskii@advcake.com>
2022-01-02 20:24:46 +0200

    try deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'kanape',
@@ -60,4 +60,6 @@ export default {
       '~assets/styles/typography.sass',
     ]
   },
+
+  telemetry: false
 }

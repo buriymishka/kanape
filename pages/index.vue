ddc1a0a98b47866d010ec27d5c87d57816e4f5d0
Mikhail Uglianskii <mikhail.uglianskii@advcake.com>
2022-01-02 18:45:07 +0200

    clear

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <Tutorial/>
+  <div>
+    <h1>Сайт посвященный котику и собачке</h1>
+    <div class="wrapper">
+      <img src="https://sun9-25.userapi.com/impg/O_y7qWOcvKaynrKTYHxZN4TAh6d0MqNcQVDDHw/uUHFGWb41E4.jpg?size=1200x1600&quality=96&sign=d004ac7eeb5fc265be7f9f58ef950d08&type=album" alt="">
+      <img src="https://sun9-63.userapi.com/impg/g11_bJpqbzFc2xedZOHuKYKnTuq54Cz86qOJYQ/trt8oqLX5sU.jpg?size=1200x1600&quality=96&sign=8fdb3d10be052689e045fbaa0d45730e&type=album" alt="">
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+
 </script>
+
+<style lang="sass" scoped>
+.wrapper
+  display: flex
+  img
+    max-width: 50%
+</style>
